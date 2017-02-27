04016f06bcf0cd30a3fb81af59391762ed3b28f7
Lewis Levin <lewis@neilson-levin.org>
2017-02-27 12:49:17 -0800

    Performance opt for regularization

--- general_nn.jl
+++ general_nn.jl
@@ -1,25 +1,39 @@
 # generalizing sigmoid neural networks for up to 5 layers
 
-# TODO Find a better way to handle bias
+# TODO Enable test only runs with saved thetas
 
 using MAT
 using Devectorize
 using Plots
-pyplot()
+pyplot()  # initialize the backend used by Plots
 
 # This is a quicker way to call general_nn with only 1 hidden layer with n_hid units.
-function general_nn(matfname::String, n_iters::Int64, n_hid::Int64, alpha=0.35, 
-    classify="sigmoid", compare=false)
-    general_nn(matfname, n_iters,[n_hid], alpha, classify, compare)
+function general_nn(matfname::String, n_iters::Int64, n_hid::Int64, alpha=0.35, mb_size=0,
+    lambda=0, classify="softmax", compare=false)
+    general_nn(matfname, n_iters,[n_hid], alpha, mb_size, lambda, classify, compare)
 end
 
 function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alpha=0.35,
-    classify="sigmoid", compare=false)
+    mb_size=0, lambda=0, classify="softmax", compare=false)
     # creates a nn with 1 input layer, up to 3 hidden layers with n_hid units,
-    # and a single output logistic neuron to do 1-way classification
+    # and output units matching the dimensions of the training data y
     # returns theta
 
-    # digits5000by784.mat",2000, [400,400], .4, sigmoid   .913 on test set
+    # some outcomes:
+        # digits5000by784.mat",2000, [400,400], .4, sigmoid   .913 on test set
+
+        # general_nn("digits10000by784.mat", 500, [300,300], .35, 500, "softmax");
+        # Fraction correct labels predicted training: 0.9959
+        # Final cost training: 0.045472118645165066
+        # Fraction correct labels predicted test: 0.9294
+        # Final cost test: 0.23052762259087423
+
+        # general_nn("digits5000by784.mat", 1500, 190,.3, 500, .07);
+        # Fraction correct labels predicted training: 1.0
+        # Final cost training: 0.05618060065676693
+        # Fraction correct labels predicted test: 0.941
+        # Final cost test: 0.14601174228898248
+
 
     # layers must be 1, 2, or 3 hidden layers. In addition, there will always be
     # 1 input layer and 1 output layer. So, total layers will be 3, 4, or 5.
@@ -31,6 +45,7 @@ function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alp
 
     # read the data, load into variables, set some useful variables
     df = matread(matfname)
+
     # Split into train and test datasets, if we have them
     if in("train", keys(df))
         inputs = df["train"]["x"]'  # set examples as columns to optimize for julia column-dominant operations
@@ -39,23 +54,46 @@ function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alp
         inputs = df["x"]'
         targets = df["y"]'
     end
-
     k,m = size(inputs) # number of features k by no. of examples m
     t = size(targets,1) # number of output units
     n_hid_layers = size(n_hid, 1)
     output_layer = 2 + n_hid_layers # input layer is 1, output layer is highest value
     
-    # weight dims include bias term the 2nd index at each layer
-    #    this follows the convention for theta: the outputs to the current layer activation
-    #    are rows of theta and the inputs from the next layer down are columns
-    w_dims = [[k, 1]] # "weight" dimensions for the input layer--not used
+    #setup mini-batch
+        if mb_size == 0
+            mb_size = m  # cause 1 mini-batch with all of the samples
+        elseif mb_size > m
+            mb_size = m
+        elseif mb_size < 1
+            mb_size = 1
+        elseif mod(m, mb_size) != 0
+            error("Mini-batch size $mb_size does not divide into samples $m.")
+        end
+        n_mb = Int(m / mb_size)
+
+        if mb_size < m
+            # randomize order of samples: 
+                # labels in training data sometimes in a block, which will make
+                # mini-batch train badly because a batch might not contain mix of labels
+            sel_index = randperm(m)
+            inputs[:] = inputs[:, sel_index]
+            targets[:] = targets[:, sel_index]  
+        end
+
+        mb_inputs = zeros(size(inputs,1), mb_size)  # pre-allocate mini-batch of inputs
+        mb_targets = zeros(size(targets,1), mb_size)  # pre-allocate mini-batch of targets
+
+    # theta dimensions for each layer of the neural network 
+    #    this follows the convention that the outputs to the current layer activation
+    #    are rows of theta and the inputs from the next layer below are columns
+    theta_dims = [[k, 1]] # "weight" dimensions for the input layer--not used
     for i = 2:output_layer-1
-        push!(w_dims, [n_hid[i-1], w_dims[i-1][1]+1])
+        push!(theta_dims, [n_hid[i-1], theta_dims[i-1][1]+1])  # 2nd index at each layer includes bias term
     end
-    push!(w_dims, [t, n_hid[end]+ 1]) # weight dimensions for the output layer
+    push!(theta_dims, [t, n_hid[end]+ 1]) # weight dimensions for the output layer
 
     # use either sigmoid or softmax for output layer with multiple classification
-    if w_dims[output_layer][1] > 1  # more than one output (unit)
+    if theta_dims[output_layer][1] > 1  # more than one output (unit)
         if classify == "sigmoid"
             class_function = sigmoid
         elseif classify == "softmax"
@@ -69,40 +107,53 @@ function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alp
 
     # setup cost
     cost_function = cross_entropy_cost
-    cost_history = zeros(n_iters+1)
+    mb_cost_history = zeros(n_iters * n_mb)
 
     # theta = weight matrices for all calculated layers (except the input layer)
     # initialize random weight parameters including bias term
     theta = [zeros(2,2)] # initialize collection of 2d float arrays: input layer 1 not used
     interval = 0.5 # random weights will be generated in [-interval, interval]
     for i = 2:output_layer
-        push!(theta, rand(w_dims[i]...) .* (2.0 * interval) .- interval)
+        push!(theta, rand(theta_dims[i]...) .* (2.0 * interval) .- interval)
     end
 
-    # initialize matrices used in training by layer to enable update-in-place for speed
-    a, a_wb, z = initialize_feedfwd(inputs, targets, theta, output_layer, m)
+    # pre-allocate matrices used in training by layer to enable update-in-place for speed
+    a, a_wb, z = preallocate_feedfwd(inputs, targets, theta, output_layer, m)
+    mb_a, mb_a_wb, mb_z = preallocate_feedfwd(mb_inputs, mb_targets, theta, output_layer, mb_size)
 
-    # initialize matrices for back propagation
+    # initialize matrices for back propagation to enable update-in-place for speed
     eps = [rand(2,2)]  # not used at input layer
     delta = [rand(2,2)]  # not used at input layer
     for i = 2:output_layer
-        push!(eps, zeros(size(a[i])))
+        push!(eps, zeros(size(mb_a[i])))
         push!(delta, zeros(size(theta[i])))
     end
 
-    # train the neural network and accumulate cost_history
-    for i = 1:n_iters+1
-        feedfwd!(theta, targets, output_layer, class_function,
-            a, a_wb, z)
-        predictions = a[output_layer]
-        cost_history[i] = cost_function(targets, predictions, m)
-        backprop!(theta, targets, output_layer, alpha, m, a, a_wb, z, eps, delta)
+    # train the neural network and accumulate mb_cost_history
+    for i = 1:n_iters  # loop for "epochs"
+        for j = 1:n_mb  # loop for mini-batches
+            start = (j - 1) * mb_size + 1
+            fin = start + mb_size - 1
+            mb_a[1][:] = inputs[:,start:fin]  # input layer activation for mini-batch
+            mb_a_wb[1][:] = vcat(ones(1,mb_size), mb_a[1])
+            mb_targets[:] = targets[:, start:fin]         
+
+            feedfwd!(theta, mb_targets, output_layer, class_function,
+                mb_a, mb_a_wb, mb_z)
+            backprop!(theta, mb_targets, output_layer, lambda, alpha, 
+                mb_size, mb_a, mb_a_wb, mb_z, eps, delta)
+
+            predictions = mb_a[output_layer]
+            mb_cost_history[(i-1)*n_mb+j] = cost_function(mb_targets, predictions, 
+                mb_size, theta, lambda, output_layer)
+        end
     end
     
     # output some statistics 
     # training data
-    println("Fraction correct labels predicted training: ", score(targets, a[output_layer]))
-    println("Final cost training: ", cost_history[end-1])
+    predictions = feedfwd!(theta, targets, output_layer, class_function, a, a_wb, z)
+    println("Fraction correct labels predicted training: ", score(targets, predictions))
+    println("Final cost training: ", mb_cost_history[end-1])
 
     # test statistics
     if in("test", keys(df))
@@ -111,15 +162,15 @@ function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alp
         testm = size(inputs,2)
 
         #feedforward for test data
-        a, a_wb, z = initialize_feedfwd(inputs, targets, theta, output_layer, testm)
+        a, a_wb, z = preallocate_feedfwd(inputs, targets, theta, output_layer, testm)
         feedfwd!(theta, targets, output_layer, class_function, a, a_wb, z)
         predictions = a[output_layer]
         println("Fraction correct labels predicted test: ", score(targets, predictions))
-        println("Final cost test: ", cost_function(targets, predictions, m))
+        println("Final cost test: ", cost_function(targets, predictions, m, theta, lambda, output_layer))
     end
 
     # plot the progress of training cost
-    plot(cost_history[1:end-1], lab="Training Cost", ylims=(0, Inf))
+    plot(mb_cost_history[1:end], lab="Mini-batch Training Cost", ylims=(0, Inf))
     gui()
 
     if compare
@@ -130,7 +181,7 @@ function general_nn(matfname::String, n_iters::Int64, n_hid::Array{Int64,1}, alp
 end
 
 
-function initialize_feedfwd(inputs, targets, theta, output_layer, m)
+function preallocate_feedfwd(inputs, targets, theta, output_layer, m)
     a = [inputs]
     a_wb = [vcat(ones(1, m), inputs)] # input layer with bias column, never changed in loop
     z = [zeros(2,2)] # not used for input layer
@@ -145,6 +196,10 @@ function initialize_feedfwd(inputs, targets, theta, output_layer, m)
 end
 
 
+function build_feedfwd_views(inputs, targets, theta, output_layer, m)
+    follows
+end
+
 function feedfwd!(theta, targets, output_layer, class_function,
     a, a_wb, z)
     # modifies a, a_wb, z in place
@@ -154,7 +209,7 @@ function feedfwd!(theta, targets, output_layer, class_function,
     # x[:] enables replace in place--reduce allocations, speed up loop
 
     # feed forward from inputs to output layer predictions
-    @fastmath for ii = 2:output_layer-1
+    @fastmath for ii = 2:output_layer-1  # ii is the current layer
         z[ii][:] = theta[ii] * a_wb[ii-1]
         a[ii][:] = sigmoid(z[ii])
         a_wb[ii][2:end, :] = a[ii]  
@@ -164,13 +219,13 @@ function feedfwd!(theta, targets, output_layer, class_function,
 end
 
 
-function backprop!(theta, targets, output_layer, alpha, m, a, a_wb, z, eps, delta)
+function backprop!(theta, targets, output_layer, lambda, alpha, m, a, a_wb, z, eps, delta)
     # modifies theta, eps, delta in place
     # use for iterations in training
     # send it all of the data or a mini-batch
     # receives intermediate storage of a, a_wb, z, eps, delta to reduce memory allocations
 
-    for jj = output_layer:-1:2
+    for jj = output_layer:-1:2  # jj is the current layer
         if jj == output_layer
             eps[jj][:] = a[jj] .- targets  # eps is epsilon, this is the output layer
         else
@@ -178,15 +233,29 @@ function backprop!(theta, targets, output_layer, alpha, m, a, a_wb, z, eps, delt
         end
         delta[jj][:] = eps[jj] * a_wb[jj-1]'
         alphaoverm = alpha / m  # need this because @devec won't do division. 
-        @devec gradterm = theta[jj] .- alphaoverm .* delta[jj]
+        lamoverm = lambda / m  # need this because @devec won't do division. 
+        if lambda == 0
+            @devec gradterm = theta[jj] .- alphaoverm .* delta[jj] 
+        else
+            @devec gradterm = (1 - lamoverm) .* (theta[jj] .-  (alphaoverm .* delta[jj])) 
+        end
         theta[jj][:] = gradterm
     end
 end
 
 
-function cross_entropy_cost(targets, predictions, m)
+function cross_entropy_cost(targets, predictions, m, theta, lambda, output_layer)
     @fastmath cost = -1.0 / m * sum(targets .* log(predictions) + 
-            (1.0 .- targets) .* log(1.0 .- predictions))
+        (1.0 .- targets) .* log(1.0 .- predictions))
+    if lambda != 0
+        lamover2m = lambda / (2 * m)
+        regterm = 0.0
+        for i in 2:output_layer  # is this faster than the comprehension?
+            @fastmath regterm = regterm + lamover2m .* sum(theta[i,2:end] .* theta[i,2:end]) 
+        end
+        # @fastmath regterm = lambda/(2*m)*sum([sum(theta[i][:, 2:end] .^ 2) for i in 2:output_layer])
+        cost = cost + regterm
+    end
     return cost
 end
 
